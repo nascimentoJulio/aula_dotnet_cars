889f245739f14c72e433a3845c90d8e9692b75be
afonsolelis <afonsolelis@gmail.com>
2024-01-11 19:31:04 -0300

    first commit

---
+++ Volkswagen.Dashboard.Services/ICarsService.cs
@@ -0,0 +1,9 @@
+﻿namespace Volkswagen.Dashboard.Services
+{
+    public interface ICarsService
+    {
+        int CreateCar(CarModel carModel);
+        CarModel GetCarById(int id);
+        List<CarModel> GetCars();
+    }
+}
---
+++ Volkswagen.Dashboard.WebApi/Controllers/CarController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.OpenApi.Any;
+using Volkswagen.Dashboard.Services;
+
+namespace Volkswagen.Dashboard.WebApi.Controllers
+{
+    [Route("api/car")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly ICarsService _carsService;
+
+        public CarController(ICarsService carsService)
+        {
+            _carsService = carsService;
+        }
+
+        [HttpGet]
+        public IActionResult GetCar()
+        {
+            return Ok(_carsService.GetCars());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCar([FromRoute] int id)
+        {
+            var car = _carsService.GetCarById(id);
+            if(car is null)
+                return NotFound("Carro não encontrado!");
+
+            return Ok(car);
+        }
+
+        [HttpPost]
+        public IActionResult CreateCar([FromBody] CarModel carModel)
+        {
+            var id = _carsService.CreateCar(carModel);
+
+            return Created("api/car", id);
+        }
+    }
+}
